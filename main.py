dd0f34f039df4dc8e5ce4186b4bfb66de6548ed6
Aaron Mazel-Gee <aaron1729@gmail.com>
2023-11-08 02:59:28 -0500

    add another cycle, but not totally functioning

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-
-# env_vars = open(".env", "r").read().splitlines()
-# for str in env_vars:
-#     if str[:15] == "openai_api_key=":
-#         openai_api_key = str[15:]
-
 from openai import OpenAI
 client = OpenAI()
 
@@ -11,11 +5,15 @@ from datetime import datetime
 now = str(datetime.now())
 timestamp = now[:10] + "_" + now[11:13] + "-" + now[14:16] + "-" + now[17:19]
 
+#####
+
 destination = "kyoto"
 
 
 system_prompt = open("prompts/" + destination + "_system_prompt.txt", "r").read()
 user_prompts = open("prompts/" + destination + "_user_prompts.txt", "r").read().split("*****")
+
+system_message_obj = {"role": "user", "content": system_prompt}
 
 user_message_objs = []
 
@@ -25,15 +23,23 @@ for user_prompt in user_prompts:
 
 
 
-
 completion = client.chat.completions.create(
   model="gpt-4-32k",
-  messages=[{"role": "system", "content": system_prompt}, ] + user_message_objs
+  messages=[system_message_obj, user_message_objs[0]]
+)
+
+
+completion_1 = client.chat.completions.create(
+    model="gpt-4-32k",
+    messages=[system_message_obj, user_message_objs[1]]
 )
 
 
 
 story = open("stories/" + destination + "_" + timestamp + ".txt", "w")
+
+
+
 
 # actually it seems that there's only one element here, as of 2023-11-08 at ~2:30am.
 # if it does ever become an array, a different way of writing it all out at once would be the following, although make sure to add extra newlines if needed.
@@ -41,6 +47,10 @@ story = open("stories/" + destination + "_" + timestamp + ".txt", "w")
 
 for obj in completion.choices:
     print("writing a message...")
-    story.write(completion.choices[i].message.content + "\n\n")
+    story.write(obj.message.content + "\n\n")
+
+for obj in completion_1.choices:
+    print("writing a message_1...")
+    story.write(obj.message.content + "\n\n")
 
 print("finished writing")
