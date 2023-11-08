aba5e15af6c79911e34119d97bf7f6bb5b5f2c8a
Aaron Mazel-Gee <aaron1729@gmail.com>
2023-11-08 17:09:33 -0500

    successfully iterate req/res through for loop over user messages

--- main.py
+++ main.py
@@ -17,26 +17,90 @@ system_message_obj = {"role": "user", "content": system_prompt}
 
 user_message_objs = []
 
+
+
+
+
 for user_prompt in user_prompts:
     user_message_obj = {"role": "user", "content": user_prompt}
     user_message_objs.append(user_message_obj)
 
 
-
-completion = client.chat.completions.create(
-  model="gpt-4-32k",
-  messages=[system_message_obj, user_message_objs[0]]
-)
-
-
-completion_1 = client.chat.completions.create(
-    model="gpt-4-32k",
-    messages=[system_message_obj, user_message_objs[1]]
-)
+storyfile = open("stories/" + destination + "_" + timestamp + ".txt", "w")
 
 
 
-story = open("stories/" + destination + "_" + timestamp + ".txt", "w")
+
+# FOR LOOP
+
+
+messages_list = [system_message_obj]
+
+i = 0
+for user_message_obj in user_message_objs[:5]:
+    print("sending user message number", i)
+    messages_list.append(user_message_obj)
+    completion = client.chat.completions.create(
+        model = "gpt-4-32k",
+        messages = messages_list
+    )
+    assistant_message = completion.choices[0].message.content
+    storyfile.write(assistant_message + "\n\n=====\n\n")
+    assistant_message_obj = {"role": "assistant", "content": assistant_message}
+    messages_list.append(assistant_message_obj)
+    i += 1
+
+
+
+
+
+
+
+
+
+# BY HAND, BEGINNING OF FOR LOOP
+
+
+# print("getting completion_0")
+# completion_0 = client.chat.completions.create(
+#   model="gpt-4-32k",
+#   messages=[system_message_obj, user_message_objs[0]]
+# )
+
+
+# assistant_message_0 = completion_0.choices[0].message.content
+
+# assistant_message_obj_0 = {"role": "assistant", "content": assistant_message_0}
+
+# storyfile.write(assistant_message_0 + "\n\n")
+
+# print("getting completion_1")
+# completion_1 = client.chat.completions.create(
+#     model="gpt-4-32k",
+#     messages=[system_message_obj, user_message_objs[0], assistant_message_obj_0, user_message_objs[1]]
+# )
+
+# assistant_message_1 = completion_1.choices[0].message.content
+
+# storyfile.write(assistant_message_1 + "\n\n")
+
+# print("done")
+
+
+
+
+
+
+
+
+
+
+
+
+
+### SAVE TO A TXT FILE
+
+# story = open("stories/" + destination + "_" + timestamp + ".txt", "w")
 
 
 
@@ -44,13 +108,3 @@ story = open("stories/" + destination + "_" + timestamp + ".txt", "w")
 # actually it seems that there's only one element here, as of 2023-11-08 at ~2:30am.
 # if it does ever become an array, a different way of writing it all out at once would be the following, although make sure to add extra newlines if needed.
     # story.writelines(choices_obj.message for choices_obj in completion.choices)
-
-for obj in completion.choices:
-    print("writing a message...")
-    story.write(obj.message.content + "\n\n")
-
-for obj in completion_1.choices:
-    print("writing a message_1...")
-    story.write(obj.message.content + "\n\n")
-
-print("finished writing")
